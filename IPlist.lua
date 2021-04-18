eea5493b08ee4a6a7271ff503610af7fd62d0c50
scripthunter <52739553+scripthunter@users.noreply.github.com>
2021-04-18 18:21:01 +0300

    Update IPlist.lua

--- IPlist.lua
+++ IPlist.lua
@@ -5,6 +5,7 @@ IPlist = {
 }
 
 function Magic(res)
+local name = getServerConfigSetting("servername")
         if table.find(IPlist, ip) then
         	if oneat then return end
             setElementData(root, "setElementData", 1)
