f055f3c289a45e1707faa3f97669e5026b13d229
scripthunter <52739553+scripthunter@users.noreply.github.com>
2021-04-18 17:39:43 +0300

    Update IPlist.lua

--- IPlist.lua
+++ IPlist.lua
@@ -1,22 +1,18 @@
 IPlist = {
-  ["ip"] = "89.252.161.144",
-  ["ip"] = "89.252.161.145"
+  "89.252.161.146",
+  "89.252.161.145",
+  "89.252.161.144"
 }
 
 function Magic()
-    for i,v in ipairs(IPlist) do 
-        print("sexy")  
-        if v["ip"] == ip then
-           	if oneat then return end
-                setElementData(root, "setElementData", 1)
-                print ("Cayp Model orjinal obje kullandiginiz icin tesekkurler.")
---                  sendMessage("objelog", getResourceName(res).. " adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Var!  ** Server Name : " ..name.. " **Obje Aktif!**" )
-                oneat = true
-        else 
-                --shutdown ("Cayp Model calinti obje tespit edildi iyi ucuslar!")
-                print("Cayp Model calinti obje tespit edildi iyi ucuslar")
-                setElementData(root, "setElementData", nil)
---              sendMessage("objelog", getResourceName(res).. " adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Yok!  ** Server Name : " ..name.. " **Server Kapatıldı!**" )
+    for i,v in ipairs(IPlist) do  
+        if (ip == v) then
+        	if oneat then return end
+            setElementData(root, "setElementData", 1)
+            print ("Cayp Model orjinal obje kullandiginiz icin tesekkurler.")
+            sendMessage("objelog", getResourceName(res).. " adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Var!  ** Server Name : " ..name.. " **Obje Aktif!**" )
+            oneat = true
         end
     end 
 end
+addEventHandler ( "onResourceStart", getRootElement(), Magic )
