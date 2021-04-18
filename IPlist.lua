7716d9687c7667913b6bdf24414d4f68d28bbcd9
scripthunter <52739553+scripthunter@users.noreply.github.com>
2021-04-18 18:45:58 +0300

    Update IPlist.lua

--- IPlist.lua
+++ IPlist.lua
@@ -3,20 +3,24 @@ IPlist = {
   "89.252.161.145",
   "89.252.161.144"
 }
-
+oneat = nil
 function Magic(res)
-name = getServerConfigSetting("servername")
-        if table.find(IPlist, ip) then
+    name = getServerConfigSetting("servername")
+    fetchRemote("https://api.my-ip.io/ip", function(ip)
+        print("IP Kontrol ediliyor....")
+        serverip = ip 
+        if table.find(IPlist, serverip) then
         	if oneat then return end
             setElementData(root, "setElementData", 1)
             print ("Cayp Model orjinal obje kullandiginiz icin tesekkurler.")
             sendMessage("objelog", getResourceName(res).. " adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Var!  ** Server Name : " ..name.. " **Obje Aktif!**" )
             oneat = true
         else 
-                --shutdown ("Cayp Model calinti obje tespit edildi iyi ucuslar!")
+            shutdown ("Cayp Model calinti obje tespit edildi iyi ucuslar!")
             print("Cayp Model calinti obje tespit edildi iyi ucuslar")
             setElementData(root, "setElementData", nil)
             sendMessage("objelog", getResourceName(res).. " adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Yok!  ** Server Name : " ..name.. " **Server Kapatıldı!**" )
         end
+    end)    
 end
 addEventHandler ( "onResourceStart", getRootElement(), Magic )
